30a4f9d6cf868479a94d84f64034c63c9ad70f45
Arthur Feeney <afeeney@uci.edu>
2026-01-31 12:24:44 -0800

    fix imports for refactored architectures

--- bubbleformer/models/__init__.py
+++ bubbleformer/models/__init__.py
@@ -1,10 +1,5 @@
-from .neighbor_moe_fluid_embed import NeighborMoEFluidEmbed
-from .neighbor_moe import NeighborMoE
-from .neighbor_vit import NeighborViT
-from .axial_vit import AxialViT
-from .axial_moe import AxialMoE
-from .vit import ViT
-from .vit_moe import ViTMoE
+from .vit import ViT, AxialViT, NeighborViT
+from .moe import ViTMoE, AxialMoE, NeighborMoE
 from .unets import ModernUnet, ClassicUnet
 from ._api import (
     register_model,
