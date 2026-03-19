3de75fba7c240905338161a3fa57a3498ccc056c
Arthur Feeney <afeeney@uci.edu>
2026-03-19 16:28:08 -0700

    Add a trapezoidal LR schedule. This is popular in training speedruns.

--- bubbleformer/utils/lr_schedulers.py
+++ bubbleformer/utils/lr_schedulers.py
@@ -1,4 +1,4 @@
-from torch.optim.lr_scheduler import CosineAnnealingLR, LambdaLR, SequentialLR
+from torch.optim.lr_scheduler import CosineAnnealingLR, LambdaLR, SequentialLR, ConstantLR, LinearLR
 
 
 class CosineWarmupLR(SequentialLR):
@@ -27,5 +27,39 @@ class CosineWarmupLR(SequentialLR):
                 ),
             ],
             milestones=[warmup_iters],
+            last_epoch=last_epoch,
+        )
+
+class TrapezoidalLR(SequentialLR):
+    """
+    Trapezoidal learning rate scheduler
+    Args:
+        optimizer (torch.optim.Optimizer): Optimizer
+        warmup_iters (int): Number of warmup iterations
+        max_iters (int): Maximum number of iterations
+        eta_min (float): Minimum learning rate
+    """
+    def __init__(self, optimizer, scale_factor, warmup_iters, flat_iters, cooldown_iters, eta_min=0, last_epoch=-1):
+        super().__init__(
+            optimizer,
+            schedulers=[
+                LinearLR(
+                    optimizer,
+                    start_factor=scale_factor,
+                    total_iters=warmup_iters,
+                ),
+                ConstantLR(
+                    optimizer,
+                    factor=1.0,
+                    total_iters=flat_iters,
+                ),
+                LinearLR(
+                    optimizer,
+                    start_factor=1.0,
+                    end_factor=scale_factor,
+                    total_iters=cooldown_iters,
+                ),
+            ],
+            milestones=[warmup_iters, flat_iters + warmup_iters],
             last_epoch=last_epoch,
         )
